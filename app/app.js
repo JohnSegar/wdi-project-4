dfaeee51c10dd1f889761a931068bb54eebbbdac
JohnSegar <johnjamessegar@hotmail.co.uk>
2016-05-30 11:27:07 +0100

    about to do Grunt

--- app/app.js
+++ app/app.js
@@ -52,4 +52,10 @@ app.use(function (err, req, res, next) {
 var routes = require('./config/routes');
 app.use("/api", routes);
 
+app.use(express.static(__dirname + "/public"));
+
+app.use("/*", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.listen(config.port);
