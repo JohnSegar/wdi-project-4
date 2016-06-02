5149e9b864f6d9f580b5380599bdf1a8e65356a6
JohnSegar <johnjamessegar@hotmail.co.uk>
2016-06-02 14:36:40 +0100

    animations

--- public/js/app.js
+++ public/js/app.js
@@ -56917,15 +56917,22 @@ angular
 
   UsersSameGameController.$inject = ['$state', 'User', 'CurrentUser'];
   function UsersSameGameController($state, User, CurrentUser){
-    self.users = getUsers();
+    self.getUsers = getUsers;
+    self.users = [];
+
 
     function getUsers(){
+      console.log("ksdfjg");
       var id = CurrentUser.getUser()._id;
       return User.search({ id: id }, function(response) {
-        console.log(response);
+        console.log(response.users);
+        self.users = response.users;
       });
     }
 
+    getUsers();
+
+    return self;
   }
 
 angular
